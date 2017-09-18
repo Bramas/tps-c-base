b9590d62708a4475b23a45980e0a5b7ddb581eeb
Quentin Bramas <quentin.bramas@gmail.com>
2017-09-18 13:47:47 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ popd
 echo 'export LD_LIBRARY_PATH=/usr/local/lib' >> $HOME/.bashrc
 echo 'export CC=clang' >> $HOME/.bashrc
 echo 'export LDLIBS="-lcrypt -lm -ltps -lSDL2 -lSDL2_ttf"' >> $HOME/.bashrc
-echo 'export CFLAGS="-fsanitize=integer -fsanitize=undefined -ggdb3 -O0 -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wshadow"' >> $HOME/.bashrc
+echo 'export CFLAGS="-DTPS_IN_BROWSER -fsanitize=integer -fsanitize=undefined -ggdb3 -O0 -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wshadow"' >> $HOME/.bashrc
 echo 'alias tps-debug=debug50' >> $HOME/.bashrc
 echo 'alias open="c9 open"' >> $HOME/.bashrc
 echo 'alias indent="indent -kr -ci2 -cli2 -i2 -l80 -nut "' >> $HOME/.bashrc
